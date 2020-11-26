72cc3a49847642b5f734f7e74562ba2aa3022f07
yaacine <gy_zidelmal@esi.dz>
2020-11-26 11:43:16 +0100

    chore: extracred 995 articles of Byung Kyu Kim

--- test.py
+++ test.py
@@ -3,28 +3,45 @@ from scholarly import scholarly, ProxyGenerator
 import json
 import psycopg2
 
-# Retrieve the author's data, fill-in, and print
-search_query = scholarly.search_author('Steven A Cholewiak')
-author = next(search_query).fill()
 
+authors = []
+authors.append('Byung Kyu Kim')
+authors.append('Steven A Cholewiak')
+authors.append('Jan Feijen')
+authors.append('PWM Blom')
+authors.append('Katharina Landfester')
+authors.append('Kurt Kremer')
+authors.append('Qiang Fu')
 
-# Take a closer look at the first publication
-pubs = author.publications
-dict_data = []
-csv_file = "test.csv"
+csv_columns = ['id','abstract','author','cites','cites_id','journal','number','pages','publisher','title','url','volume','year']
+csv_file = "Articles.csv"
+counter=0
+for author_name in authors:
+    try:
+        with open(csv_file, 'w') as csvfile:
+            writer = csv.DictWriter(csvfile, fieldnames=csv_columns, extrasaction='ignore')
+            writer.writeheader()
 
-for pub in pubs:
-    pub_filled = pub.fill()
-    print(pub_filled)
-    dict_data.append(pub_filled.bib)
+            # Retrieve the author's data, fill-in, and print
+            search_query = scholarly.search_author(author_name)
+            author = next(search_query).fill()
 
-csv_columns = ['abstract','author','cites','cites_id','journal','number','pages','publisher','title','url','volume','year']
+            # Take a closer look at the first publication
+            pubs = author.publications
+            dict_data = []
+            
+            for idx,pub in enumerate(pubs):
+                pub_filled = pub.fill()
+                print(pub_filled)
+                pub_filled.bib['id']= idx
+                dict_data.append(pub_filled.bib)
+                writer.writerow(pub_filled.bib)
+                counter += counter
+                
+    except IOError:
+        print("I/O error")
+        
+    
 
-try:
-    with open(csv_file, 'w') as csvfile:
-        writer = csv.DictWriter(csvfile, fieldnames=csv_columns, extrasaction='ignore')
-        writer.writeheader()
-        for data in dict_data:
-            writer.writerow(data)
-except IOError:
-    print("I/O error")
+  
+    
