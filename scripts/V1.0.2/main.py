5cd3ccb4491b912ec00be205c0ba33c7fd1996c5
yaacine <gy_zidelmal@esi.dz>
2021-01-05 16:01:42 +0100

    fix: citations file

--- scripts/V1.0.2/main.py
+++ scripts/V1.0.2/main.py
@@ -5,8 +5,9 @@ import controller.publications as pubs
 # author.extract_authors()
 
 # pubs.get_papers_for_author("4Hme0r8AAAAJ")
+pubs.extract_papers_from_authors()
 
-pubs.get_papers_from_paper_citations("Climate change 2007-the physical science basis: Working group I contribution to the fourth assessment report of the IPCC")
+# pubs.get_papers_from_paper_citations("Climate change 2007-the physical science basis: Working group I contribution to the fourth assessment report of the IPCC")
 # author.extract_coauthors_sequencially("4Hme0r8AAAAJ")
 
 # author.get_coauthors("8csgm4cAAAAJ")
