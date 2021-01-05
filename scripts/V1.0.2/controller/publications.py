dac5ee0b5c121391ac577d1b4d77e542252215a7
yaacine <gy_zidelmal@esi.dz>
2021-01-05 17:52:37 +0100

    feat: scrapipng done

--- scripts/V1.0.2/controller/publications.py
+++ scripts/V1.0.2/controller/publications.py
@@ -3,7 +3,7 @@ from scholarly import scholarly, ProxyGenerator
 from .keyword_manger import mark_line_as_done, get_next_keyword
 from .csv_manager import  write_author, insert_co_authering, write_publication, get_authors_dataframe, update_authors_dataframe, insert_citation , get_publications_dataframe, update_publications_dataframe
 
-PUBLICATIONS_CSV_FILE = 'scripts/V1.0.2/datasets/articles/articles.csv'
+PUBLICATIONS_CSV_FILE = 'scripts/V1.0.2/datasets/articles/articles2.csv'
 AUTHORS_CSV_FILE = 'scripts/V1.0.2/datasets/authors/authors.csv'
 CITATIONS_CSV_FILE = 'scripts/V1.0.2/datasets/citations/citations.csv'
 
@@ -39,8 +39,12 @@ def extract_papers_from_authors():
     for index, row in df.iterrows():
         if row['got_publications'] == 0:
             print(row['got_publications'])
-            get_papers_for_author(row['scholar_id'])
-            row['got_publications'] = 1
+            try:
+                get_papers_for_author(row['scholar_id'])
+            except Exception as identifier:
+                row['got_publications'] = 1               
+                update_authors_dataframe(df)
+    row['got_publications'] = 1               
     update_authors_dataframe(df)
 
 
@@ -99,36 +103,61 @@ def publication_to_dict(publication):
             ',', '.')
     else:
         publication_dict['title'] = ''
+
     if 'pub_year' in publication['bib'].keys():
         publication_dict['pub_year'] = publication['bib']['pub_year']
     else:
         publication_dict['pub_year'] = ''
+
     if 'author' in publication['bib'].keys():
         publication_dict['author'] = publication['bib']['author']
     else:
         publication_dict['author'] = ''
+
     if 'volume' in publication['bib'].keys():
         publication_dict['volume'] = publication['bib']['volume']
     else:
         publication_dict['volume'] = ''
+
+
+    if 'journal' in publication['bib'].keys():
+        publication_dict['journal'] = publication['bib']['journal']
+    else:
+        publication_dict['journal'] = ''
+
+    if 'number' in publication['bib'].keys():
+        publication_dict['number'] = publication['bib']['number']
+    else:
+        publication_dict['number'] = ''
+
+
+    if 'pages' in publication['bib'].keys():
+        publication_dict['pages'] = publication['bib']['pages']
+    else:
+        publication_dict['pages'] = ''
+
     if 'publisher' in publication['bib'].keys():
         publication_dict['publisher'] = publication['bib']['publisher'].replace(
             ',', '.')
     else:
         publication_dict['publisher'] = ''
+
     if 'abstract' in publication['bib'].keys():
         publication_dict['abstract'] = publication['bib']['abstract'].replace(
             ',', '.')
     else:
         publication_dict['abstract'] = ''
+
     if 'filled' in publication.keys():
         publication_dict['filled'] = publication['filled']
     else:
         publication_dict['filled'] = ''
+
     if 'author_pub_id' in publication.keys():
         publication_dict['author_pub_id'] = publication['author_pub_id']
     else:
         publication_dict['author_pub_id'] = ''
+
     if 'num_citations' in publication.keys():
         publication_dict['num_citations'] = publication['num_citations']
     else:
@@ -145,6 +174,10 @@ def publication_to_dict(publication):
         publication_dict['citedby_url'] = publication['citedby_url']
     else:
         publication_dict['citedby_url'] = ''
+    if 'author_id' in publication.keys():
+        publication_dict['author_id'] = ' | '.join(publication['author_id'])
+    else:
+        publication_dict['author_id'] = ''
     if 'eprint_url' in publication.keys():
         publication_dict['eprint_url'] = publication['eprint_url']
     else:
