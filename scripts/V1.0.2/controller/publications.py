8397af95afeec41b06ca185ba91ff178a2effd94
yaacine <gy_zidelmal@esi.dz>
2021-01-07 16:59:21 +0100

    chore:prepring scrapping 3 with gh actions

--- scripts/V1.0.2/controller/publications.py
+++ scripts/V1.0.2/controller/publications.py
@@ -3,7 +3,7 @@ from scholarly import scholarly, ProxyGenerator
 from .keyword_manger import mark_line_as_done, get_next_keyword
 from .csv_manager import  write_author, insert_co_authering, write_publication, get_authors_dataframe, update_authors_dataframe, insert_citation , get_publications_dataframe, update_publications_dataframe
 import time
-PUBLICATIONS_CSV_FILE = 'scripts/V1.0.2/datasets/articles/articles3.csv'
+PUBLICATIONS_CSV_FILE = 'scripts/V1.0.2/datasets/articles/articles4.csv'
 AUTHORS_CSV_FILE = 'scripts/V1.0.2/datasets/authors/authors2.csv'
 CITATIONS_CSV_FILE = 'scripts/V1.0.2/datasets/citations/citations.csv'
 
@@ -39,9 +39,9 @@ def extract_papers_from_authors():
     PERIOD_OF_TIME = 18000 # 5hours
     df = get_authors_dataframe(AUTHORS_CSV_FILE)
     for index, row in df.iterrows():
-        if time.time() > start + PERIOD_OF_TIME : 
-            exit(0)
-            break
+        # if time.time() > start + PERIOD_OF_TIME : 
+        #     exit(0)
+        #     break
         if row['got_publications'] == 0:
             print(row['got_publications'])
             try:
