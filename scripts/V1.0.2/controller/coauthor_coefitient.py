f88fd1449e48d0e98e8fe4c94ea01bd83c5f58d5
yaacine <gy_zidelmal@esi.dz>
2021-01-31 14:52:32 +0100

    chore: started scrapping pubs of author3.csv

--- scripts/V1.0.2/controller/coauthor_coefitient.py
+++ scripts/V1.0.2/controller/coauthor_coefitient.py
@@ -6,6 +6,27 @@ from datetime import datetime
 import os 
 
 
+# get unique time for author file name
+now = datetime.now().time()  # time object
+print("now =", now)
+
+# get unique time for author file name (used when scrapping authors based on the co-authors relationship)
+now = datetime.now()
+current_time = now.strftime("%H:%M:%S")
+
+publication_file_name_output = 'articles'+str(now).replace(' ', '_')+'.csv'
+PUBLICATIONS_CSV_FILE_OUTPUT_WITH_IDS = os.path.join(
+    'scripts', 'V1.0.2', 'datasets', 'clean_articles', publication_file_name_output)
+# create the file if is does not exist
+print(PUBLICATIONS_CSV_FILE_OUTPUT_WITH_IDS)
+os.makedirs(os.path.dirname(
+    PUBLICATIONS_CSV_FILE_OUTPUT_WITH_IDS), exist_ok=True)
+
+PUBLICATIONS_CSV_FILE_INPUT = 'scripts/V1.0.2/datasets/articles/articles.csv'
+
+ARTICLES_INPUT_FOLDER = 'scripts/V1.0.2/datasets/articles'
+ARTICLES_OUTPUT_FOLDER = 'scripts/V1.0.2/datasets/clean_articles'
+
 
 
 def get_number_pubs_authors(scholar_id):
