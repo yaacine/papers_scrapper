9e62079e8acb9881654b81f5c3ef9d854607e702
yaacine <gy_zidelmal@esi.dz>
2021-01-31 15:24:25 +0100

    feat: update authors file

--- scripts/V1.0.2/controller/coauthor_coefitient.py
+++ scripts/V1.0.2/controller/coauthor_coefitient.py
@@ -14,18 +14,19 @@ print("now =", now)
 now = datetime.now()
 current_time = now.strftime("%H:%M:%S")
 
-publication_file_name_output = 'articles'+str(now).replace(' ', '_')+'.csv'
-PUBLICATIONS_CSV_FILE_OUTPUT_WITH_IDS = os.path.join(
-    'scripts', 'V1.0.2', 'datasets', 'clean_articles', publication_file_name_output)
+author_file_name_output = 'author_nbpubs_'+str(now).replace(' ', '_')+'.csv'
+AUTHORS_CSV_FILE_OUTPUT_WITH_NBPUBS = os.path.join(
+    'scripts', 'V1.0.2', 'datasets','authors','authors_nbpubs', publication_file_name_output)
 # create the file if is does not exist
-print(PUBLICATIONS_CSV_FILE_OUTPUT_WITH_IDS)
+print(AUTHORS_CSV_FILE_OUTPUT_WITH_NBPUBS)
 os.makedirs(os.path.dirname(
-    PUBLICATIONS_CSV_FILE_OUTPUT_WITH_IDS), exist_ok=True)
+    AUTHORS_CSV_FILE_OUTPUT_WITH_NBPUBS), exist_ok=True)
 
-PUBLICATIONS_CSV_FILE_INPUT = 'scripts/V1.0.2/datasets/articles/articles.csv'
+PUBLICATIONS_FOLDER_INPUT = 'scripts/V1.0.2/datasets/articles/articles.csv'
 
 ARTICLES_INPUT_FOLDER = 'scripts/V1.0.2/datasets/articles'
-ARTICLES_OUTPUT_FOLDER = 'scripts/V1.0.2/datasets/clean_articles'
+AUTHORS_INPUT_FOLDER = 'scripts/V1.0.2/datasets/authors'
+AUTHORS_OUTPUT_FOLDER = 'scripts/V1.0.2/datasets/authors_nbpubs'
 
 
 
@@ -33,3 +34,36 @@ def get_number_pubs_authors(scholar_id):
     """
         Gets the number of publications of an author based on the dataset of the project
     """
+    
+
+
+def get_articles_files_list(directory_in_str):
+    file_list = []
+    directory = os.fsencode(directory_in_str)
+    for file in os.listdir(directory):
+        filename = os.fsdecode(file)
+        if filename.endswith(".csv"):
+            file_list.append(filename)
+            continue
+        else:
+            continue
+    return file_list
+
+
+
+"""
+### the following seciton adds the missing columns to the publications csv files
+"""
+
+
+def add_columns_to_authors():
+    files_list = get_authors_files_list(AUTHORS_INPUT_FOLDER)
+    for file in files_list:
+        add_columns_to_publications_file(file)
+
+
+def add_columns_to_author_file(file_name):
+    file_path = os.path.join(AUTHORS_OUTPUT_FOLDER, file_name)
+    df = pd.read_csv(file_path)
+    df["nb_pubs"] = 0
+    df.to_csv(file_path, index=False)
