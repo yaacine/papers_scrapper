a3ad29ac6040b14620aba8ad8b2f2f3905e75896
yaacine <gy_zidelmal@esi.dz>
2021-02-07 14:56:58 +0100

    feat: added script that scripts interests of authors

---
+++ scripts/V1.0.2/controller/author_interests_filler.py
@@ -0,0 +1,42 @@
+import csv
+from scholarly import scholarly, ProxyGenerator
+from keyword_manger import mark_line_as_done, get_next_keyword
+from csv_manager import write_author, insert_co_authering, get_authors_dataframe, update_authors_dataframe, update_last_scrapped_author_id_coauthoring
+from datetime import datetime
+import os
+
+"""
+ #############################
+ Extract the interests of the authors
+ #############################
+"""
+
+AUTHORS_CSV_FILE_INPUT_INTERESTS = 'scripts/V1.0.2/datasets/authors/authors06:36:25.csv'
+
+
+def extract_interests(input_output_file):
+    df = get_authors_dataframe(input_output_file)
+    for index, row in df.iterrows():
+        if row['interests'] in ("", None):
+            print("Getting interests of author :" + row['scholar_id'])
+            try:
+                author = scholarly.search_author_id(row['scholar_id'])
+                interests = '|'.join(author['interests'])
+                df.at[index, 'interests'] = interests
+                update_authors_dataframe(input_output_file, df)
+            except Exception as identifier:
+                print(
+                    "An exception happened while getting interests of : " + row['scholar_id'])
+                print(identifier)
+
+
+
+print("Started connection to tor !")
+
+pg = ProxyGenerator()
+pg.Tor_Internal(tor_cmd='tor')
+scholarly.use_proxy(pg)
+
+print("Connection to tor done successfully !")
+
+extract_interests(AUTHORS_CSV_FILE_INPUT_INTERESTS)
