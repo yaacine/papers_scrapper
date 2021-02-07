7d8671c335d61a8cc3e73d5bed872edca8cdead6
yaacine <gy_zidelmal@esi.dz>
2021-02-07 17:07:18 +0100

    fix: get interests of authors

--- scripts/V1.0.2/controller/author_interests_filler.py
+++ scripts/V1.0.2/controller/author_interests_filler.py
@@ -4,6 +4,7 @@ from keyword_manger import mark_line_as_done, get_next_keyword
 from csv_manager import write_author, insert_co_authering, get_authors_dataframe, update_authors_dataframe, update_last_scrapped_author_id_coauthoring
 from datetime import datetime
 import os
+import pandas as pd
 
 """
  #############################
@@ -18,25 +19,31 @@ def extract_interests(input_output_file):
     print("file readed successfully")
     for index, row in df.iterrows():
         print('interest====>'+  str(row['interests']))
-        if row['interests'] in ("", None):
+        if row['interests'] in ("", None) or pd.isna(row['interests']):
             print("Getting interests of author :" + row['scholar_id'])
             try:
                 author = scholarly.search_author_id(row['scholar_id'])
                 interests = '|'.join(author['interests'])
+                print ("new interests ====>" + str(interests))
+                #convert the type of interests into str
+                df= df.astype({"interests": str})
                 df.at[index, 'interests'] = interests
+                print("new author line ====>" )
+                print(df.at[index])
                 update_authors_dataframe(input_output_file, df)
             except Exception as identifier:
                 print(
                     "An exception happened while getting interests of : " + row['scholar_id'])
                 print(identifier)
-
+                update_authors_dataframe(input_output_file, df)
+            update_authors_dataframe(input_output_file, df)
 
 
 print("Started connection to tor !")
 
-pg = ProxyGenerator()
-pg.Tor_Internal(tor_cmd='tor')
-scholarly.use_proxy(pg)
+# pg = ProxyGenerator()
+# pg.Tor_Internal(tor_cmd='tor')
+# scholarly.use_proxy(pg)
 
 print("Connection to tor done successfully !")
 
