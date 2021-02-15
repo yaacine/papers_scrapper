abe2e50b7df51b28a51742e1cc7286ebf10890ca
yaacine <gy_zidelmal@esi.dz>
2021-02-15 06:47:39 +0100

    chore: got interests and publications

--- scripts/V1.0.2/controller/author_interests_filler.py
+++ scripts/V1.0.2/controller/author_interests_filler.py
@@ -12,12 +12,14 @@ import pandas as pd
  #############################
 """
 
-AUTHORS_CSV_FILE_INPUT_INTERESTS = 'scripts/V1.0.2/datasets/authors/authors08:28:03.csv'
+AUTHORS_CSV_FILE_INPUT_INTERESTS = 'scripts/V1.0.2/datasets/authors/authors08:35:38.csv'
 
 # authors08:15:21.csv
 # authors08:23:48.csv
 # authors08:26:12.csv
 # authors08:28:03.csv
+# parallel
+# authors08:35:38.csv
 def extract_interests(input_output_file):
     df = get_authors_dataframe(input_output_file)
     df = df.astype({"interests": str})
