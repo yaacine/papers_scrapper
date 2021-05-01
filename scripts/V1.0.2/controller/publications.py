4c2e9819ecffcba9dd8eba6fd194c39f1fd6fa7c
yaacine <gy_zidelmal@esi.dz>
2021-05-01 14:50:21 +0100

    started working on scripts/V1.0.2/datasets/ben_m/authors_la_suite/bk16_authors.csv

--- scripts/V1.0.2/controller/publications.py
+++ scripts/V1.0.2/controller/publications.py
@@ -25,11 +25,11 @@ print('file created')
 
 PUBLICATIONS_CSV_FILE_INPUT = 'scripts/V1.0.2/datasets/articles/articles2.csv'
 # AUTHORS_CSV_FILE = 'scripts/V1.0.2/datasets/authors/authors3.csv'
-AUTHORS_CSV_FILE = 'scripts/V1.0.2/datasets/authors/authors08:00:56.csv'
+AUTHORS_CSV_FILE = 'scripts/V1.0.2/datasets/ben_m/authors_la_suite/bk16_authors.csv'
 CITATIONS_CSV_FILE = 'scripts/V1.0.2/datasets/citations/citations.csv'
 COUNTER_CONFIG_FILE = "scripts/V1.0.2/datasets/counter.ini"
 
-NB_MAX_PAPERS_PER_AUTHOR = 25
+NB_MAX_PAPERS_PER_AUTHOR = 10
 
 
 def get_papers_for_author(author_id):
